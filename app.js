61ca789baa9c9af65d87819cdfa29a0d68679c01
MaxumisOD <67334727+MaxumisOD@users.noreply.github.com>
2023-02-28 00:07:11 -0600

    CSS-animation-broken

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 luxy.init();
+let scrollInfo = {
+  currentScrollTop: 0,
+  lastScrollTop: 0,
+  vector: 0,
+};
+
+document.addEventListener("scroll", scrollCheck);
+
+function scrollCheck() {
+  scrollInfo.currentScrollTop = document.documentElement.scrollTop;
+  if (scrollInfo.currentScrollTop > scrollInfo.lastScrollTop) {
+    scrollInfo.vector = 1;
+  }
+  if (scrollInfo.currentScrollTop < scrollInfo.lastScrollTop) {
+    scrollInfo.vector = -1;
+  }
+  scrollInfo.lastScrollTop = scrollInfo.currentScrollTop;
+  console.log(scrollInfo.vector);
+  window.requestAnimationFrame(render);
+}
+
+function render() {
+  const element = document.querySelector(".spotLight").style;
+  console.log("hi");
+  element.setProperty("--current-pos", element.transform);
+}
