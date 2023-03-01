52f2eef2334fde45c39069e526761ab2b9c1c319
Joshua Turner <jdturner1976@gmail.com>
2023-02-28 23:28:07 -0600

    graphing-vertex

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 luxy.init();
 let animationLoopInfo = {
-  time: 30,
+  time: 100,
   newX: 0,
   newY: 0,
   oldX: 0,
@@ -22,41 +22,30 @@ bounce
     delay: 65,
     stiffness: 2,
   });
-for (i = 0; i < 10; )
-  bounce
-    .scale({
-      from: { x: 1, y: 1 },
-      to: { x: 1, y: 1 },
-      easing: "sway",
-      duration: 300,
-      delay: 30,
-    })
-    .translate({
-      from: { x: 0, y: 0 },
-      to: { x: -100, y: 20 },
-      duration: 2000,
-      stiffness: 4,
-      delay: 3195,
-    });
+for (let axisX = 0; axisX > -200; axisX--) {
+  let exponent = Math.pow(axisX + 100, 2);
+  let axisY = (-1 / (window.innerWidth / 10)) * exponent + 100;
+  animationLoopInfo.newX = axisX - animationLoopInfo.oldX;
+  animationLoopInfo.newY = axisY - animationLoopInfo.oldY;
+  animationLoopInfo.oldY = axisY;
+  animationLoopInfo.oldX = axisX;
 
-bounce.define("my-animation");
+  bounce.translate({
+    from: { x: 0, y: 0 },
+    to: { x: animationLoopInfo.newX, y: animationLoopInfo.newY },
+    duration: 2000,
+    stiffness: 4,
+    delay: animationLoopInfo.time,
+  });
+  animationLoopInfo.time += 3;
+  console.log("test");
+}
+bounce.applyTo(document.querySelectorAll("#ball"));
+console.log(window.innerWidth);
+
+1250;
 
 // //Problems to solve
 // using graphing to create a loop downwards (will need to make it so it the euqasion can change depending on the width of the screen)
 // using vw to make the landing spots consistant for sizes
 //looping the graph
-
-for (let axisX = 100; axisX > -100; x++) {
-  let axisY = (-1 / 100) * (axisX * axisX) + 100;
-  animationLoopInfo.newX = axisX - animationLoopInfo.oldX;
-  animationLoopInfo.newY = axisY - animationLoopInfo.oldY;
-
-  bounce.translate({
-    from: { x: 0, y: 0 },
-    to: { x: 0, y: 0 },
-    duration: 2000,
-    stiffness: 4,
-    delay: time,
-  });
-  time = 1;
-}
